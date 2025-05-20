611eb48df5256b022b1ac925c770ddf3d028acf4
Miyashita Hiroshi <hiroshi.miyashita@globis.co.jp>
2025-05-20 20:15:16 +0900

    index.js change

--- index.js
+++ index.js
@@ -1,57 +1,41 @@
 const express = require('express');
 const app = express();
 
-
-async function reqHandler(req, res){
-  try {
-    res.status(200).send(JSON.stringify(req.body));
-  } catch (err){
-    res.status(500).send(JSON.stringify({"msg":"error"}));
-  }
-  return;  
-}
-
-// async function getMethodHandler(req, res){
-//   try {
-//     res.status(200).send();
-//   } catch (err){
-//     res.status(500).send(JSON.stringify({"msg":"error"}));
-//   }
-//   return;
-// };
-
-// async function postMethodHandler(req, res){
-//   try {
-//     res.status(200).send(JSON.stringify(req.body));
-//   } catch (err){
-//     res.status(500).send(JSON.stringify({"msg":"error"}));
-//   }
-//   return;
-// };
-
 app.use((req, res, next) => {
   try {
     res.charset = 'utf-8';
     res.type("application/json; charset=utf-8");
   } catch (err){
-    return res.status(500).send(JSON.stringify({"msg":"error"}));
+    //return res.status(500).send(JSON.stringify({"msg":"error"}));
+    return next(err);
   }
   next();
 });
-
 app.use(express.json());
-app.use((err, req, res, next) => {
-  if (err) {
-    return res.status(500).send(JSON.stringify({"msg":"error"}));
-  }
-  next();
-});
 
-app.route("/*")
-.all(reqHandler)
-  // .get(getMethodHandler)
-  // .post(postMethodHandler)
-;
+async function reqHandler(req, res, next){
+  try {
+    if(req.method === "GET"){
+      res.status(200).send();
+    }else{
+      res.status(200).send(JSON.stringify(req.body));
+    }
+  } catch (err){
+    return next(err);
+    //res.status(500).send(JSON.stringify({"msg":"error"}));
+  }
+  //return;  
+}
+
+app.route("/*").all(reqHandler);
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) return next(err);
+  //if (err) {
+    res.status(500).send(JSON.stringify({"msg":"error"}));
+  // }
+  // next();
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Express Start Port:${PORT}`));
