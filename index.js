ed04dbbf6812cf5dcd6d5d0d48b01dc228bcd35d
Miyashita Hiroshi <hiroshi.miyashita@globis.co.jp>
2025-05-20 20:00:48 +0900

    index.js change

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 const express = require('express');
 const app = express();
 
-async function getMethodHandler(req, res){
-  try {
-    res.status(200).send();
-  } catch (err){
-    res.status(500).send(JSON.stringify({"msg":"error"}));
-  }
-  return;
-};
 
-async function postMethodHandler(req, res){
+async function reqHandler(req, res){
   try {
     res.status(200).send(JSON.stringify(req.body));
   } catch (err){
     res.status(500).send(JSON.stringify({"msg":"error"}));
   }
-  return;
-};
+  return;  
+}
+
+// async function getMethodHandler(req, res){
+//   try {
+//     res.status(200).send();
+//   } catch (err){
+//     res.status(500).send(JSON.stringify({"msg":"error"}));
+//   }
+//   return;
+// };
+
+// async function postMethodHandler(req, res){
+//   try {
+//     res.status(200).send(JSON.stringify(req.body));
+//   } catch (err){
+//     res.status(500).send(JSON.stringify({"msg":"error"}));
+//   }
+//   return;
+// };
 
 app.use((req, res, next) => {
   try {
-//    res.set("Cache-Control", "no-store, no-cache, must-revalidate, private");
     res.charset = 'utf-8';
-//    res.removeHeader("X-Powered-By");
     res.type("application/json; charset=utf-8");
-//    res.setHeader("X-Process-Start-Time",process.hrtime.bigint().toString());
   } catch (err){
     return res.status(500).send(JSON.stringify({"msg":"error"}));
   }
@@ -35,15 +42,15 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use((err, req, res, next) => {
   if (err) {
-    res.status(500).send(JSON.stringify({"msg":"error"}));
-    return;
+    return res.status(500).send(JSON.stringify({"msg":"error"}));
   }
   next();
 });
 
 app.route("/*")
-  .get(getMethodHandler)
-  .post(postMethodHandler)
+.all(reqHandler)
+  // .get(getMethodHandler)
+  // .post(postMethodHandler)
 ;
 
 const PORT = process.env.PORT || 8080;
